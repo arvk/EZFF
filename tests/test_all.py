48be618f56510fc60daa85067f4fe5b4bddeacb2
Aravind Krishnamoorthy <kris658@usc.edu>
2019-03-08 16:42:19 -0500

    Initial batch of tests against the new structure distortion error calculation

---
+++ tests/test_all.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.append('..')
+import ezff
+from ezff.interfaces import gulp
+import pytest
+
+# TEST error_structure_distortion
+def test_structure_distortion_nobox():
+    test_value = gulp.error_structure_distortion('gulp_output_files/out.mote2.nobox', relax_atoms=True, relax_cell=False)
+    ground_truth = pytest.approx(5.171,0.01)
+    assert (test_value == ground_truth)
+
+def test_structure_distortion_nooptim():
+    test_value = gulp.error_structure_distortion('gulp_output_files/out.mote2.nooptim', relax_atoms=False, relax_cell=False)
+    ground_truth = pytest.approx(0.0,0.01)
+    assert (test_value == ground_truth)
+
+def test_structure_distortion_optim():
+    test_value = gulp.error_structure_distortion('gulp_output_files/out.mote2.optim', relax_atoms=True, relax_cell=False)
+    ground_truth = pytest.approx(0.11,0.01)
+    assert (test_value == ground_truth)
+
+def test_structure_distortion_sheared():
+    test_value = gulp.error_structure_distortion('gulp_output_files/out.mote2.sheared', relax_atoms=True, relax_cell=True)
+    ground_truth = pytest.approx(26.48,0.01)
+    assert (test_value == ground_truth)
+
+def test_structure_distortion_lj():
+    test_value = gulp.error_structure_distortion('gulp_output_files/out.lj.optim', relax_atoms=True, relax_cell=True)
+    ground_truth = pytest.approx(0.03,0.5)
+    assert (test_value == ground_truth)
