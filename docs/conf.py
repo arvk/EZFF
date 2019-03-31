bf40f0b953363f55cd7b6fb615d7a258397be632
Aravind Krishnamoorthy <kris658@usc.edu>
2019-03-31 16:56:26 -0700

    Remove other dependencies for docs

--- docs/conf.py
+++ docs/conf.py
@@ -66,7 +66,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['mpi4py','xtal', 'platypus']
+MOCK_MODULES = ['mpi4py','xtal','platypus','platypus.types','platypus.operators']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
