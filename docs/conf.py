6a446c5a8f85779ac884208e6c84b9ede5064b3c
Aravind Krishnamoorthy <kris658@usc.edu>
2019-03-31 16:49:46 -0700

    Small bugfix for docs build

--- docs/conf.py
+++ docs/conf.py
@@ -66,7 +66,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['mpi4py','xtal']
+MOCK_MODULES = ['mpi4py','xtal', 'platypus']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
