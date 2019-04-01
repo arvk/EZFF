788f81eb2e6030b7234288573f7144d2f721ec4f
Aravind Krishnamoorthy <kris658@usc.edu>
2019-04-01 13:34:43 -0700

    More coherent methods for the QChem class

--- ezff/interfaces/qchem.py
+++ ezff/interfaces/qchem.py
@@ -3,57 +3,51 @@ import numpy as np
 import xtal
 from ezff.utils import convert_units as convert
 
-def read_ground_state(outfilename):
+def read_structure(outfilename):
     """
-    Read-in a single converged structure from a QChem optimization run
+    Read-in a multiple partially-converged structures from a PES scan (including bond-scans, angle-scans and dihedral-scans)
 
-    :param outfilename: Filename for ``stdout`` from the QChem job
+    :param outfilename: Single filename for ``stdout`` from the QChem PES scan job or a list of filenames for ``stdout`` files from partial QChem PES scan jobs
     :type outfilename: str
-    :returns: ``xtal`` tranjectory with a single snapshot with the converged structure and ground-state energy
+
+    :returns: ``xtal`` trajectory object with structures and converged energies along the PES scan as individual snapshots
     """
     structure = xtal.AtTraj()
-    snapshot = structure.create_snapshot(xtal.Snapshot)
-    outfile = open(outfilename,'r')
+    energies = []
 
-    # Read energies
-    for line in outfile:
-        if 'Final energy is' in line:
-            energy_in_Hartrees = float(line.strip().split()[-1])
-            break
+    if isinstance(outfilename, list):
+        listfilenames = outfilename
+    else:
+        listfilenames = [outfilename]
 
-    snapshot.energy = energy_in_Hartrees * convert.energy['Ha']['eV']
-
-    for line in outfile:
-        if 'OPTIMIZATION CONVERGED' in line:
-            dummyline = outfile.readline()
-            dummyline = outfile.readline()
-            dummyline = outfile.readline()
-            dummyline = outfile.readline()
-            # Atomic coordinates start here
-            while True:
-                coords = outfile.readline()
-                if coords.strip()=='':
-                    break
-                atom = snapshot.create_atom(xtal.Atom)
-                atom.element, atom.cart = coords.strip().split()[1], np.array(list(map(float,coords.strip().split()[2:5])))
-            break
-
-    for line in outfile:
-        if 'Ground-State Mulliken Net Atomic Charges' in line:
-            dummyline = outfile.readline()
-            dummyline = outfile.readline()
-            dummyline = outfile.readline()
-            for atom in snapshot.atomlist:
-                atom.charge = outfile.readline().strip().split()[2]
-            break
-
-    outfile.close()
-
+    for single_outfilename in list(listfilenames):
+        # Read structure
+        outfile = open(single_outfilename,'r')
+        while True:
+            line = outfile.readline()
+            if not line: # break at EOF
+                break
+            if 'OPTIMIZATION CONVERGED' in line:
+                snapshot = structure.create_snapshot(xtal.Snapshot)
+                dummyline = outfile.readline()
+                dummyline = outfile.readline()
+                dummyline = outfile.readline()
+                dummyline = outfile.readline()
+                # Atomic coordinates start here
+                while True:
+                    coords = outfile.readline()
+                    if coords.strip()=='':
+                        break
+                    atom = snapshot.create_atom(xtal.Atom)
+                    atom.element, atom.cart = coords.strip().split()[1], np.array(list(map(float,coords.strip().split()[2:5])))
+        outfile.close()
     return structure
 
 
 
-def read_scan(outfilename):
+
+
+def read_energy(outfilename):
     """
     Read-in a multiple partially-converged structures from a PES scan (including bond-scans, angle-scans and dihedral-scans)
 
@@ -78,10 +72,49 @@ def read_scan(outfilename):
                 energy_in_Hartrees = float(line.strip().split()[-1])
                 energies.append(energy_in_Hartrees * convert.energy['Ha']['eV'])
         outfile.close()
+    return energies
 
+
+
+
+
+
+def read_atomic_charges(outfilename):
+    """
+    Read-in a multiple partially-converged structures from a PES scan (including bond-scans, angle-scans and dihedral-scans)
+
+    :param outfilename: Single filename for ``stdout`` from the QChem PES scan job or a list of filenames for ``stdout`` files from partial QChem PES scan jobs
+    :type outfilename: str
+
+    :returns: ``xtal`` trajectory object with structures and converged energies along the PES scan as individual snapshots
+    """
+    structure = xtal.AtTraj()
+    energies = []
+
+    if isinstance(outfilename, list):
+        listfilenames = outfilename
+    else:
+        listfilenames = [outfilename]
+
+    for single_outfilename in list(listfilenames):
         # Read structure
         outfile = open(single_outfilename,'r')
-        for line in outfile:
+        while True:
+            line = outfile.readline()
+            if not line: # break at EOF
+                break
+
+            if 'Ground-State Mulliken Net Atomic Charges' in line:
+                charge_array = []
+                dummyline = outfile.readline()
+                dummyline = outfile.readline()
+                dummyline = outfile.readline()
+                while True:
+                    charges = outfile.readline().strip().split()
+                    if charges[0][0] == '-':
+                        break
+                    charge_array.append(float(charges[2]))
+
             if 'OPTIMIZATION CONVERGED' in line:
                 snapshot = structure.create_snapshot(xtal.Snapshot)
                 dummyline = outfile.readline()
@@ -89,16 +122,15 @@ def read_scan(outfilename):
                 dummyline = outfile.readline()
                 dummyline = outfile.readline()
                 # Atomic coordinates start here
+                atomID = 0
                 while True:
                     coords = outfile.readline()
                     if coords.strip()=='':
                         break
                     atom = snapshot.create_atom(xtal.Atom)
                     atom.element, atom.cart = coords.strip().split()[1], np.array(list(map(float,coords.strip().split()[2:5])))
+                    atom.charge = charge_array[atomID]
+                    atomID += 1
 
         outfile.close()
-
-    for counter, snapshot in enumerate(structure.snaplist):
-        snapshot.energy = energies[counter]
-
     return structure
