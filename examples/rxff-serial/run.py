fd2c8c0abbeb53857912e0ebcafe7a20cb49da66
Aravind Krishnamoorthy <kris658@usc.edu>
2019-04-02 13:52:41 -0700

    New reaxff serial example

--- examples/rxff-serial/run.py
+++ examples/rxff-serial/run.py
@@ -1,19 +1,11 @@
-import sys
-sys.path.append('../..')
 import ezff
-import ezff.ffio as ffio
 from ezff.interfaces import gulp, qchem
-import xtal
-import numpy as np
-from ezff.utils import reax_forcefield
-import time
-import logging
-logging.basicConfig(level=logging.INFO)
 
-def convert_reax_forcefield(oldfile):
-    newfile = oldfile+'.lib'
-    reax_forcefield(oldfile).write_gulp_library(newfile)
-    return newfile
+# Define ground truths
+gt_gs = qchem.read_structure('ground_truths/optCHOSx.out')
+gt_gs_energy = qchem.read_energy('ground_truths/optCHOSx.out')
+gt_scan = qchem.read_structure('ground_truths/scanCHOSx.out')
+gt_scan_energy = qchem.read_energy('ground_truths/scanCHOSx.out')
 
 def my_error_function(rr):
     # Get a unique path for GULP jobs from the MPI rank. Set to '0' for serial jobs
@@ -22,58 +14,41 @@ def my_error_function(rr):
     except:
         path = '0'
 
-    # Calculate_Ground_State
-    gt_gs = qchem.read_ground_state('ground_truths/optCHOSx.out')
-    gt_gs_energy = gt_gs.snaplist[0].energy
+    # Calculate Ground State
     md_gs_job = gulp.job(path = path)
-    ffio.write_forcefield_file(md_gs_job.path+'/HH',template,rr,verbose=False)
-    md_gs_job.forcefield = md_gs_job.path + '/HH'
-    md_gs_job.temporary_forcefield = False
     md_gs_job.structure = gt_gs
-    md_gs_job.pbc = False
+    md_gs_job.forcefield = ezff.generate_forcefield(template, rr, FFtype = 'reaxff')
+    md_gs_job.options['pbc'] = False
     md_gs_job.options['relax_atoms'] = False
     md_gs_job.options['relax_cell'] = False
-    md_gs_job.write_script_file(convert_reax_forcefield)
+    # Run GULP calculation
     md_gs_job.run(command='gulp')
-    md_gs_energy = gulp.read_energy(md_gs_job.path + '/' + md_gs_job.outfile)
+    # Read output from completed GULP job and clean-up
+    md_gs_energy = md_gs_job.read_energy()
     md_gs_job.cleanup()
 
-    # Calculate Length Scan
-    gt_scan = qchem.read_scan('ground_truths/scanCHOSx.out')
-    md_scan_energies = []
-    gt_scan_energies = []
-    for snapID, snapshot in enumerate(gt_scan.snaplist):
-        gt_scan_energies.append(snapshot.energy)
-        md_scan_job = gulp.job(path = path)
-        ffio.write_forcefield_file(md_scan_job.path+'/HH',template,rr,verbose=False)
-        thisstructure = xtal.AtTraj()
-        thisstructure.snaplist.append(snapshot)
-        md_scan_job.forcefield = md_scan_job.path + '/HH'
-        md_scan_job.temporary_forcefield = False
-        md_scan_job.structure = thisstructure
-        md_scan_job.pbc = False
-        md_scan_job.options['relax_atoms'] = False
-        md_scan_job.options['relax_cell'] = False
-        md_scan_job.write_script_file(convert_reax_forcefield)
-        md_scan_job.run(command='gulp')
-        md_scan_energy = gulp.read_energy(md_scan_job.path + '/' + md_scan_job.outfile)
-        md_scan_energies.append(md_scan_energy)
-        md_scan_job.cleanup()
-        del md_scan_job
+
+    # Calculate PES Scan
+    md_scan_job = gulp.job(path = path)
+    md_scan_job.structure = gt_scan
+    md_scan_job.forcefield = ezff.generate_forcefield(template, rr, FFtype = 'reaxff')
+    md_scan_job.options['pbc'] = False
+    md_scan_job.options['relax_atoms'] = False
+    md_scan_job.options['relax_cell'] = False
+    # Run GULP calculation
+    md_scan_job.run(command='gulp')
+    # Read output from completed GULP job and clean-up
+    md_scan_energy = md_scan_job.read_energy()
+    md_scan_job.cleanup()
 
     # Calculate error
-    md_scan_energies = np.array(md_scan_energies)
-    gt_scan_energies = np.array(gt_scan_energies)
-    md_gs_energy = np.array(md_gs_energy)
-    gt_gs_energy = np.array(gt_gs_energy)
-    total_error = ezff.error_PES_scan( md_scan_energies-md_gs_energy, gt_scan_energies-gt_gs_energy, weights = 'uniform')
-
+    total_error = ezff.error_energy( md_scan_energy-md_gs_energy, gt_scan_energy-gt_gs_energy, weights = 'uniform')
     return [total_error]
 
 # Read template and variable ranges
-bounds = ffio.read_variable_bounds('variable_bounds', verbose=False)
-template = ffio.read_forcefield_template('template')
+bounds = ezff.read_variable_bounds('variable_bounds', verbose=False)
+template = ezff.read_forcefield_template('template')
 
 problem = ezff.OptProblem(num_errors = 1, variable_bounds = bounds, error_function = my_error_function, template = template)
 algorithm = ezff.Algorithm(problem, 'NSGAII', population = 16)
-ezff.optimize(problem, algorithm, iterations = 3)
+ezff.optimize(problem, algorithm, iterations = 5)
