63ff1a7a770319fb13eb2e4b05d28c934a4a2ef1
Aravind Krishnamoorthy <kris658@usc.edu>
2019-04-02 14:48:16 -0700

    Added qchem_read_structure_singlepoint tests

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,6 +1,5 @@
-import sys
-sys.path.append('..')
 import ezff
+import xtal
 from ezff.interfaces import qchem
 import pytest
 import numpy as np
@@ -11,3 +10,12 @@ def test_qchem_read_energy_singlepoint():
     is_correct_size = (len(energy) == 1)
     is_correct_value = (np.linalg.norm(energy) == pytest.approx(14044.3073))
     assert (is_correct_type and is_correct_size and is_correct_value)
+
+
+def test_qchem_read_structure_singlepoint():
+    structure = qchem.read_structure('qchem_output_files/optCHOSx.out')
+    has_correct_snapsize = (len(structure.snaplist) == 1)
+    has_correct_atomsize = (len(structure.snaplist[0].atomlist) == 10)
+    has_correct_data_1 = (np.linalg.norm(structure.snaplist[0].atomlist[0].cart) == pytest.approx(1.38097616))
+    has_correct_data_2 = (np.linalg.norm(structure.snaplist[0].atomlist[0].fract) == pytest.approx(1e-12))
+    assert (has_correct_snapsize and has_correct_atomsize and has_correct_data_1 and has_correct_data_2)
