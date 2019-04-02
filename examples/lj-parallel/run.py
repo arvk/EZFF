91c301a67b6336afeb28a8e7c27c21d208915153
Aravind Krishnamoorthy <kris658@usc.edu>
2019-04-02 13:08:11 -0700

    New example for LJ-parallel

--- examples/lj-parallel/run.py
+++ examples/lj-parallel/run.py
@@ -1,18 +1,13 @@
-import sys
-sys.path.append('../..')
 import ezff
-import ezff.ffio as ffio
-from ezff.interfaces import gulp
+from ezff.interfaces import gulp, vasp
 import numpy as np
-import logging
-logging.basicConfig(level=logging.INFO)
 
-bounds = ffio.read_variable_bounds('variable_bounds', verbose=False)
-template = ffio.read_forcefield_template('template')
+bounds = ezff.read_variable_bounds('variable_bounds', verbose=False)
+template = ezff.read_forcefield_template('template')
 
 # DEFINE GROUND TRUTHS
 gt_bulk_modulus = 1.1236 #GPa
-gt_structure = ezff.read_atomic_structure('ground_truths/POSCAR')
+gt_structure = vasp.read_atomic_structure('ground_truths/POSCAR')
 
 def my_error_function(variable_values):
     # Get rank from pool
@@ -21,33 +16,31 @@ def my_error_function(variable_values):
     except:
         myrank = 0
 
-    # FOR THE RELAXED STRUCTURE
+    # Configure GULP job.
     path = str(myrank)
-    relaxed_job = gulp.job(path=path)
-    ffio.write_forcefield_file(str(myrank)+'/FF', template, variable_values, verbose=False)
-    relaxed_job.forcefield = str(myrank)+'/FF'
-    relaxed_job.temporary_forcefield = False
-    relaxed_job.structure = gt_structure
-    relaxed_job.pbc = True
-    relaxed_job.options['relax_atoms'] = True
-    relaxed_job.options['relax_cell'] = True
+    md_job = gulp.job(path=path)
+    md_job.structure = gt_structure
+    md_job.forcefield = ezff.generate_forcefield(template, variable_values, FFtype='SW')
+    md_job.options['pbc'] = True
+    md_job.options['relax_atoms'] = True
+    md_job.options['relax_cell'] = True
 
     # Submit job and read output
-    relaxed_job.write_script_file()
-    relaxed_job.run(command='gulp')
+    md_job.run()
 
     # Calculate errors in lattice constant and elastic modulus
-    moduli = gulp.read_elastic_moduli(relaxed_job.path + '/' + relaxed_job.outfile)
-    md_bulk_modulus = np.mean([moduli[i,i] for i in range(3)])
-    lattice = gulp.read_lattice_constant(relaxed_job.path + '/' + relaxed_job.outfile)
-    bulk_modulus_error = np.linalg.norm(md_bulk_modulus - gt_bulk_modulus)
-    latt_error = np.linalg.norm(lattice['err_abc'][0])
-    #relaxed_job.cleanup()  # FINISH RELAXED JOB
+    moduli = md_job.read_elastic_moduli()                                                # 6X6 elastic modulus tensor inside a list of length 1 (for a single input structure)
+    md_bulk_modulus = np.mean([moduli[0][i,i] for i in range(3)])                        # Bulk modulus is sum of diagonal elements in moduli[0]
+    bulk_modulus_error = np.linalg.norm(md_bulk_modulus - gt_bulk_modulus)               # Error is the L2 norm of deviation from the ground truth value
+
+    md_structure = md_job.read_structure()                                               # Read relaxed structure after optimization
+    error_abc, error_ang = ezff.error_lattice_constant(MD=md_structure, GT=gt_structure) # error_abc = error in lattice constants, error_ang = error in lattice angles
+    latt_error = np.linalg.norm(error_abc[0])                                            # error in 'a' lattice constant
 
     return [latt_error, bulk_modulus_error]
 
 pool = ezff.Pool()
 problem = ezff.OptProblem(num_errors = 2, variable_bounds = bounds, error_function = my_error_function, template = template)
-algorithm = ezff.Algorithm(problem, 'NSGAII', population = 256, pool = pool)
-ezff.optimize(problem, algorithm, iterations = 10)
+algorithm = ezff.Algorithm(problem, 'NSGAII', population = 32, pool = pool)
+ezff.optimize(problem, algorithm, iterations = 16)
 pool.close()
