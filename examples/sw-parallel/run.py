b88a01b13809c1f3ef7442f05c04dfa156920d2c
Aravind Krishnamoorthy <kris658@usc.edu>
2019-04-02 17:17:08 -0700

    Small updates to sw-parallel

--- examples/sw-parallel/run.py
+++ examples/sw-parallel/run.py
@@ -6,14 +6,14 @@ bounds = ezff.read_variable_bounds('variable_bounds', verbose=False)
 template = ezff.read_forcefield_template('template')
 
 # DEFINE GROUND TRUTHS
-gt_relax_disp_GM = vasp.read_phonon_dispersion('ground_truths/relaxed/GM.dat')
+gt_relax_disp_GS = vasp.read_phonon_dispersion('ground_truths/relaxed/G_S.dat')
 gt_relax_structure = vasp.read_atomic_structure('ground_truths/relaxed/POSCAR')
 gt_c11 = 160.0 #GPa for C11 of MoSe2
 
-gt_expanded_disp_GM = vasp.read_phonon_dispersion('ground_truths/expanded/GM.dat')
+gt_expanded_disp_GS = vasp.read_phonon_dispersion('ground_truths/expanded/G_S.dat')
 gt_expanded_structure = vasp.read_atomic_structure('ground_truths/expanded/POSCAR')
 
-gt_compressed_disp_GM = vasp.read_phonon_dispersion('ground_truths/compressed/GM.dat')
+gt_compressed_disp_GS = vasp.read_phonon_dispersion('ground_truths/compressed/G_S.dat')
 gt_compressed_structure = vasp.read_atomic_structure('ground_truths/compressed/POSCAR')
 
 
@@ -34,11 +34,11 @@ def my_error_function(variable_values):
     relaxed_job.options['relax_cell'] = True
     relaxed_job.options['phonon_dispersion'] = True
     relaxed_job.options['phonon_dispersion_from'] = '0 0 0'
-    relaxed_job.options['phonon_dispersion_to'] = '0.5 0 0'
+    relaxed_job.options['phonon_dispersion_to'] = '0.5 0.5 0'
     # Submit job and read output
     relaxed_job.run()
     # Read output from completed GULP job and cleanup job files
-    md_relax_disp_GM = relaxed_job.read_phonon_dispersion()
+    md_relax_disp_GS = relaxed_job.read_phonon_dispersion()
     md_relaxed_moduli = relaxed_job.read_elastic_moduli()
     md_relaxed_structure = relaxed_job.read_structure()
     relaxed_job.cleanup()  # FINISH RELAXED JOB
@@ -54,11 +54,11 @@ def my_error_function(variable_values):
     compressed_job.options['relax_cell'] = False
     compressed_job.options['phonon_dispersion'] = True
     compressed_job.options['phonon_dispersion_from'] = '0 0 0'
-    compressed_job.options['phonon_dispersion_to'] = '0.5 0 0'
+    compressed_job.options['phonon_dispersion_to'] = '0.5 0.5 0'
     # Submit job and read output
     compressed_job.run()
     # Read output from completed GULP job and cleanup job files
-    md_compressed_disp_GM = compressed_job.read_phonon_dispersion()
+    md_compressed_disp_GS = compressed_job.read_phonon_dispersion()
     compressed_job.cleanup()  # FINISH COMPRESSED JOB
 
 
@@ -72,11 +72,11 @@ def my_error_function(variable_values):
     expanded_job.options['relax_cell'] = False
     expanded_job.options['phonon_dispersion'] = True
     expanded_job.options['phonon_dispersion_from'] = '0 0 0'
-    expanded_job.options['phonon_dispersion_to'] = '0.5 0 0'
+    expanded_job.options['phonon_dispersion_to'] = '0.5 0.5 0'
     # Submit job and read output
     expanded_job.run()
     # Read output from completed GULP job and cleanup job files
-    md_expanded_disp_GM = expanded_job.read_phonon_dispersion()
+    md_expanded_disp_GS = expanded_job.read_phonon_dispersion()
     expanded_job.cleanup()  # FINISH EXPANDED JOB
 
     # Compute 5 errors from the 3 GULP jobs
@@ -87,14 +87,14 @@ def my_error_function(variable_values):
     md_c11 = md_relaxed_moduli[0][0,0] * md_relaxed_structure.box[2,2] * (2.0/13.97)  # Extracting c11 for a bulk-like layered structure from the monolayer GULP calculation
     modulus_error = np.linalg.norm(md_c11 - gt_c11)
 
-    phon_error_relaxed = ezff.error_phonon_dispersion(MD=md_relax_disp_GM, GT=gt_relax_disp_GM, weights='acoustic')
-    phon_error_expanded = ezff.error_phonon_dispersion(MD=md_expanded_disp_GM, GT=gt_expanded_disp_GM, weights='acoustic')
-    phon_error_compressed = ezff.error_phonon_dispersion(MD=md_compressed_disp_GM, GT=gt_compressed_disp_GM, weights='acoustic')
+    phon_error_relaxed = ezff.error_phonon_dispersion(MD=md_relax_disp_GS, GT=gt_relax_disp_GS, weights='acoustic')
+    phon_error_expanded = ezff.error_phonon_dispersion(MD=md_expanded_disp_GS, GT=gt_expanded_disp_GS, weights='acoustic')
+    phon_error_compressed = ezff.error_phonon_dispersion(MD=md_compressed_disp_GS, GT=gt_compressed_disp_GS, weights='acoustic')
 
     return [a_lattice_error, b_lattice_error, modulus_error, phon_error_relaxed, phon_error_compressed, phon_error_expanded]
 
 pool = ezff.Pool()
 problem = ezff.OptProblem(num_errors = 6, variable_bounds = bounds, error_function = my_error_function, template = template)
-algorithm = ezff.Algorithm(problem, 'NSGAII', population = 3, pool = pool)
+algorithm = ezff.Algorithm(problem, 'NSGAII', population = 32, pool = pool)
 ezff.optimize(problem, algorithm, iterations = 5)
 pool.close()
