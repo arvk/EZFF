1382fedd1dcd8a6d7c15e99d27ced951bc9fef03
Aravind Krishnamoorthy <kris658@usc.edu>
2019-09-03 16:59:42 -0700

    Updated tests. QChem read_structure now adds an emipirical (large) box around the cluster

--- tests/test_all.py
+++ tests/test_all.py
@@ -17,5 +17,4 @@ def test_qchem_read_structure_singlepoint():
     has_correct_snapsize = (len(structure.snaplist) == 1)
     has_correct_atomsize = (len(structure.snaplist[0].atomlist) == 10)
     has_correct_data_1 = (np.linalg.norm(structure.snaplist[0].atomlist[0].cart) == pytest.approx(1.38097616))
-    has_correct_data_2 = (np.linalg.norm(structure.snaplist[0].atomlist[0].fract) == pytest.approx(1e-12))
-    assert (has_correct_snapsize and has_correct_atomsize and has_correct_data_1 and has_correct_data_2)
+    assert (has_correct_snapsize and has_correct_atomsize and has_correct_data_1)
