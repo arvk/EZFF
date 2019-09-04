a67036df34ec0a357b44c0701d48ef4a92dbe648
Aravind Krishnamoorthy <kris658@usc.edu>
2019-09-03 17:18:12 -0700

    Revised tests. QChem read_structure now asserts based in vector distance between 2 atoms, not abosolute position of a single atom. QChem read_structure now translates a snapshot to put all atomis within the box, which causes the previous test condition to fail

--- tests/test_all.py
+++ tests/test_all.py
@@ -16,5 +16,5 @@ def test_qchem_read_structure_singlepoint():
     structure = qchem.read_structure('qchem_output_files/optCHOSx.out')
     has_correct_snapsize = (len(structure.snaplist) == 1)
     has_correct_atomsize = (len(structure.snaplist[0].atomlist) == 10)
-    has_correct_data_1 = (np.linalg.norm(structure.snaplist[0].atomlist[0].cart) == pytest.approx(1.38097616))
+    has_correct_data_1 = (np.linalg.norm(structure.snaplist[0].atomlist[0].cart - structure.snaplist[0].atomlist[1].cart) == pytest.approx(1.6389119))
     assert (has_correct_snapsize and has_correct_atomsize and has_correct_data_1)
