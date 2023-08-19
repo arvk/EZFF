1d7e3647f1f2e888c44c0965c437b4721064c918
Aravind Krishnamoorthy <akrishnamoorthy@tamu.edu>
2023-08-19 12:16:16 -0500

    Update documentation configuartion for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ copyright = '2019, Aravind Krishnamoorthy'
 author = 'Aravind Krishnamoorthy'
 
 # The short X.Y version
-version = '0.9.3'
+version = '0.9.5'
 # The full version, including alpha/beta/rc tags
 release = '0.9 Beta'
 
